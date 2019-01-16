a63ea038edb7e535899d5a038f740bc1a7226e21
Saman Daneshvar <23390018+SamanDaneshvar@users.noreply.github.com>
2019-01-16 18:18:58 -0500

    Update docstring of module

--- publead/find_lead.py
+++ publead/find_lead.py
@@ -1,5 +1,12 @@
-"""Identify the research lead in a research group, by looking at the author names in the publications of the group,
-and counting the number of papers were each author was the last author and also the second-to-last author.
+"""Identify the research lead in a research group, by looking at the author names in the publications of the group.
+
+This script loads a text file of BibTeX entries of papers. It then creates a dictionary of all authors, and counts
+the number of papers were each author is the last author and also the second-to-last author.
+In the end, it writes the dictionary into a CSV file.
+
+Remarks:
+- Papers with only 1 author won't count towards the last author count of their author.
+- Papers with only 2 authors won't count towards the second-to-last author count of their authors.
 """
 
 from publead import utils
